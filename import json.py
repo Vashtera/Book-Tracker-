a6480a585ca4fa7d26a3a4a898f5cc710f8a3dc2
vashtera <raul.aytbaev@gmail.com>
2026-05-02 12:24:16 +0500

    Start practice

---
+++ import json.py
@@ -0,0 +1,15 @@
+import json
+
+class Books:
+    def __init__(self):
+        self.books = {}
+        self.status = {}
+
+    def add_book(self, author, title):
+        pass
+    
+    def add_read_status(self, request):
+        pass
+
+    def show_unread_book(self):
+        pass
