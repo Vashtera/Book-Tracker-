ef00f1b83949d29b6f69576beb2925e985f88e6e
vashtera <raul.aytbaev@gmail.com>
2026-05-02 13:58:24 +0500

    Rewrited add_book func

--- import json.py
+++ import json.py
@@ -1,29 +1,34 @@
 import json
+import copy
 
 class Books:
     def __init__(self):
         self.books = {}
-        self.status = {}
 
     def load(self) -> None:
         try:
-            with open("data.json", 'r') as file:
+            with open("library.json", 'r') as file:
                 self.books = json.load(file)
         except FileNotFoundError:
             self.books = {}
 
     def add_book(self, author, title):
-        if title not in self.books.values():
-            self.books[author] = title
-        else:
+        if title not in self.books.values('title'):
+            self.books['Unread'] = []
+        self.books['Unread'].append({
+            'author': author,
+            'title' : title 
+            })
+        if title in self.books.values('title'):
             print(f"The {title} book is exist in your library")
     
     def add_read_status(self, request):
-        pass
+        if request == "Y":
+
 
     def show_unread_book(self):
         pass
 
     def save(self):
-        with open("data.json", 'w') as file:
+        with open("library.json", 'w') as file:
             json.dump(self.books, file)
